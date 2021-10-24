7090774c443f90ff5ce81a098066cfa8421f5674
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2021-10-24 03:50:09 -0400

    Solved subtask 3

--- challenge.py
+++ challenge.py
@@ -1,17 +1,21 @@
 # DailyProgrammer Challenge 39
-import base64
 import requests
 
+# simple input function
 def getinput () :
     return str(input("Input String: "))
 
+# The main calculator
+def mainFunc (word):
+    outputnum = 0
+    for l in word:
+        outputnum += (ord(l) - 96)
+    return outputnum
+
+#Given dictionary
+dictionary = requests.get("https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt").text.splitlines()
+
 def lettersum (intparam = 0) :
-    def mainFunc (word):
-        outputnum = 0
-        for l in word:
-            outputnum += (ord(l) - 96)
-        return outputnum
-    dictionary = requests.get("https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt").text.splitlines()
     if (intparam == 1):
         for word in dictionary:
             if (mainFunc(word) == 319):
@@ -23,8 +27,14 @@ def lettersum (intparam = 0) :
                 counter += 1
         print(counter)
     elif (intparam == 3):
+        maxlen = 0
         for word in dictionary:
-            t = "bruh" # work on this
+            if (mainFunc(word) > maxlen):
+                maxlen = mainFunc(word)
+        lengthylist = [0] * (maxlen + 1)
+        for word in dictionary:
+            lengthylist[mainFunc(word)] += 1
+        print(max(lengthylist))
     else:
         x = getinput()
         print(mainFunc(x))
@@ -34,4 +44,6 @@ def lettersum (intparam = 0) :
 # 1.
 # lettersum(1)
 # 2.
-lettersum(2)
+# lettersum(2)
+# 3.
+# lettersum(3)
